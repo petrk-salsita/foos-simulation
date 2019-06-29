d937ee68ee09f2fb2b4b2fdbca01057a169a14dd
petrkubat <petrk@salsitasoft.com>
2019-06-29 14:05:12 +0200

    Print players by Rating descending

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ matches.forEach(match => {
     const t1p2Elo = match.Team1Player2Id !== null ? playersById[match.Team1Player2Id].Rating : null;
     const t2p1Elo = playersById[match.Team2Player1Id].Rating;
     const t2p2Elo = match.Team2Player2Id !== null ? playersById[match.Team2Player2Id].Rating : null;
-    const difference = computeAveraging(t1p1Elo, t1p2Elo, t2p1Elo, t2p2Elo, match.Team1Won);
+    const difference = computeAddition(t1p1Elo, t1p2Elo, t2p1Elo, t2p2Elo, match.Team1Won);
 
     console.log(`${t1p1Elo}, ${t1p2Elo}, ${t2p1Elo}, ${t2p2Elo}`);
     console.log(difference);
@@ -32,4 +32,5 @@ matches.forEach(match => {
     }
 });
 
-console.log(playersById);
+const sortedPlayers = Object.values(playersById).sort((player1, player2) => player2.Rating - player1.Rating)
+console.log(sortedPlayers);
